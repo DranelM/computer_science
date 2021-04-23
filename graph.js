2935c351ea48f1cd52d4250e67914c6e80e9d333
Maciej Lenard <maciej.lenard1@gmail.com>
2021-04-23 21:24:57 +0200

    graph depthFirstTraversal

---
+++ graph.js
@@ -0,0 +1,55 @@
+class Graph {
+    constructor() {
+        this.adjList = {}
+    }
+
+    addNode(node) {
+        this.adjList[node] = [];
+    }
+
+    addEdge(node1, node2) {
+        this.adjList[node1].push(node2)
+        this.adjList[node2].push(node1)
+    }
+
+    depthFirstTraversal(firstNode) {
+        const stack = [];
+        const visited = {};
+
+        stack.push(firstNode);
+        visited[firstNode] = true;
+
+        while (stack.length) {
+            const current = stack.pop();
+            const neighbors = this.adjList[current];
+            console.log(current);
+            neighbors.forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    stack.push(neighbor);
+                    visited[neighbor] = true;
+                }
+            });
+        }
+    }
+}
+
+const graph = new Graph();
+graph.addNode(1);
+graph.addNode(2);
+graph.addNode(3);
+graph.addNode(4);
+graph.addNode(5);
+graph.addEdge(1, 2);
+graph.addEdge(1, 5);
+graph.addEdge(2, 5);
+graph.addEdge(2, 3);
+graph.addEdge(4, 3);
+graph.addEdge(4, 2);
+graph.addEdge(4, 5);
+
+console.log(graph);
+
+graph.depthFirstTraversal(1);
+
+
+
