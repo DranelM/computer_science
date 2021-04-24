af6de16ce6f2ce18b0f5f8d9bfb68b625fc83fed
Maciej Lenard <maciej.lenard1@gmail.com>
2021-04-24 20:39:39 +0200

    ADD: recursion to binaryTree

--- binaryTree.js
+++ binaryTree.js
@@ -1,37 +1,3 @@
-// class Tree {
-//     constructor() {
-//         this.root = null
-//     }
-
-//     add(value) {
-
-//         let current = this.root
-//         if (!current) {
-//             this.root = new Node(value)
-//             return
-//         }
-//         if (value < current.value) {
-//             if (current.left) {
-//                 current.left.add(value)
-//             } else {
-//                 current.left = new Tree(value)
-//             }
-//             return
-//         } else {
-//             if (current.right) {
-//                 current.right.add(value)
-//             } else {
-//                 current.right = new Tree(value)
-//             }
-//             return
-//         }
-//     }
-
-//     toObject() {
-//         return this.root
-//     }
-// }
-
 class Tree {
     constructor() {
         this.root = null
@@ -70,6 +36,57 @@ class Tree {
     }
 }
 
+class TreeWRecurrency {
+    constructor() {
+        this.root = null
+    }
+
+    add(value) {
+        if (!this.root) {
+            this.root = new Node(value);
+        } else {
+            this.addHelper(this.root, value);
+        }
+    }
+
+    addHelper(node, value) {
+        if (value < node.value) {
+            if (node.left) {
+                this.addHelper(node.left, value);
+            } else {
+                node.left = new Node(value);
+                return node.left;
+            }
+        } else if (value > node.value) {
+            if (node.right) {
+                this.addHelper(node.right, value);
+            } else {
+                node.right = new Node(value);
+                return node.right;
+            }
+        }
+        return;
+    }
+
+    contains(value, node = this.root) {
+        if (!node) {
+            return false
+        }
+
+        if (node.value === value) {
+            return true;
+        } else if (value < node.value) {
+            return this.contains(value, node.left);
+        } else if (value > node.value) {
+            return this.contains(value, node.right);
+        }
+    }
+
+    toObject() {
+        return this.root;
+    }
+}
+
 class Node {
     constructor(value) {
         this.value = value
@@ -85,5 +102,13 @@ tree.add(3)
 tree.add(6)
 console.log(tree)
 
+const treeWRecurrency = new TreeWRecurrency();
+treeWRecurrency.add(1)
+treeWRecurrency.add(3)
+treeWRecurrency.add(6)
+console.log(treeWRecurrency)
+
+console.log(treeWRecurrency.contains(3));
+console.log(treeWRecurrency.contains(7));
 
 
